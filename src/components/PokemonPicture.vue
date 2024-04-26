3094facf6644cddaacda34ec8105d792c0a4b196
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2024-04-26 13:36:36 +0200

    Estructura basica app

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="hidden-pokemon"
+      alt="pokemon"
+      srcset=""
+    />
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="fade-in"
+      alt="pokemon"
+      srcset=""
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 32%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
