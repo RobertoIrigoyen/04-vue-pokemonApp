400e9de527bab9b388f57820a8213e969d040377
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2024-04-30 14:44:17 +0200

    Pokemon Options Compnent testing

--- tests/unit/components/PokemonPicture.spec.js
+++ tests/unit/components/PokemonPicture.spec.js
@@ -21,26 +21,27 @@ describe('Pokemon Picture component', () => {
     })
 
     const [img1, img2] = wrapper.findAll('img')
-
     expect(img1.exists()).toBeTruthy()
     expect(img2).toBe(undefined)
     expect(img1.classes('hidden-pokemon')).toBe(true)
-    expect(img1.attributes('src')).toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/151.svg')
+    expect(img1.attributes('src')).toBe(
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/151.svg'
+    )
   })
 
   test('debe mostrar el pokemon si el showPokemon está en true', () => {
     const wrapper = shallowMount(PokemonPicture, {
-        props: {
-          pokemonId: 151,
-          showPokemon: true
-        }
-      })
-  
-      const img = wrapper.find('img')
-  
-      expect(img.exists()).toBeTruthy()
-     
-      expect(img.classes('hidden-pokemon')).toBe(false)
-      expect(img.classes('fade-in')).toBe(true)
+      props: {
+        pokemonId: 151,
+        showPokemon: true
+      }
+    })
+
+    const img = wrapper.find('img')
+
+    expect(img.exists()).toBeTruthy()
+
+    expect(img.classes('hidden-pokemon')).toBe(false)
+    expect(img.classes('fade-in')).toBe(true)
   })
 })
